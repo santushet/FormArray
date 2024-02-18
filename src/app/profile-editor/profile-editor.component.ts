3f07a7e449f4f0bb4de4ff7bc22704d1bf2ae71b
Santosh Shet <santushet@gmail.com>
2024-02-18 22:11:04 +0530

    formarray

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,8 +1,13 @@
 import { JsonPipe, NgFor } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Validators } from '@angular/forms';
-import { FormArray } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, Validators,FormArray, FormBuilder ,ReactiveFormsModule } from '@angular/forms';
+
+export interface Item {
+  firstName: string;
+  lastName: string;
+}
+
 
 @Component({
   standalone: true,
@@ -12,39 +17,76 @@ import { FormArray } from '@angular/forms';
   imports: [NgFor, ReactiveFormsModule, JsonPipe],
 })
 export class ProfileEditorComponent {
-  profileForm = this.formBuilder.group({
-    firstName: ['', Validators.required],
-    lastName: [''],
-    address: this.formBuilder.group({
-      street: [''],
-      city: [''],
-      state: [''],
-      zip: [''],
-    }),
-    aliases: this.formBuilder.array([this.formBuilder.control('')]),
-  });
 
-  get aliases() {
-    return this.profileForm.get('aliases') as FormArray;
-  }
+  ToDoListForm!: FormGroup;
+  itemsList: Item[] = [];
+
+  defaultItem = {
+    firstName: '',
+    lastName: ''
+  };
+
+  // get aliases() {
+  //   return this.profileForm.get('aliases') as FormArray;
+  // }
 
   constructor(private formBuilder: FormBuilder) {}
+  // ngOnInit(): void {
+  //   this.orderForm = new FormGroup({  
+  //     items: new FormArray([])  
+  //   });
+  // }
 
-  updateProfile() {
-    this.profileForm.patchValue({
-      firstName: 'Nancy',
-      address: {
-        street: '123 Drew Street',
-      },
+
+  // addAlias() {
+  //   this.aliases.push(this.formBuilder.control(''));
+  // }
+  get itemsArray() {
+    return <FormArray>this.ToDoListForm.get('items');
+  }
+
+  ngOnInit(): void {
+    this.ToDoListForm = this.formBuilder.group({
+      items: this.formBuilder.array([]),
+    });
+    // this.itemsList = this.serv.getmyData();
+    this.displayItems();
+  }
+
+  createItem(item: any) {
+    return this.formBuilder.group({
+      firstName: [item.firstName, [Validators.required]],
+      lastName: [item.lastName, [Validators.required]],
     });
   }
 
-  addAlias() {
-    this.aliases.push(this.formBuilder.control(''));
+  addNewItem() {
+    let formGroup = this.createItem(this.defaultItem);
+    this.itemsArray.push(formGroup);
+  }
+
+  displayItems() {
+    let transformedItems = this.itemsList.map((item: any) =>
+      this.createItem(item)
+    );
+    console.log(transformedItems);
+    this.ToDoListForm.setControl('items', this.formBuilder.array(transformedItems));
+  }
+
+  deleteItem(i: number) {
+    this.itemsArray.removeAt(i);
+  }
+
+  deleteAll() {
+    this.itemsArray.clear();
+  }
+
+  track(item: any, index: number) {
+    return index;
   }
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
-    console.warn(this.profileForm.value);
+    // console.warn(this.ToDoListForm.value);
   }
 }
